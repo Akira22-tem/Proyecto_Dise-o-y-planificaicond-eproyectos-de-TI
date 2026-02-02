38f41c6e186c19a94124c22d0e0a2d6b21a8b3b9
alexisdmc21 <admorales7@espe.edu.ec>
2026-02-02 17:00:39 -0500

    Update ProfesorDashboard.js

--- src/components/ProfesorDashboard.js
+++ src/components/ProfesorDashboard.js
@@ -4,13 +4,11 @@ import Sidebar from '../components/Sidebar';
 import ProfesorInicio from '../pages/ProfesorInicio';
 import ProfesorTareas from '../pages/ProfesorTareas';
 import ProfesorMatricula from '../pages/ProfesorMatricula';
-import Clases from '../pages/ProfesorClases';
 import ProfesorClases from '../pages/ProfesorClases';
 import { MOCK_DATA } from '../data/mockData';
 
 const ProfesorDashboard = ({ user, onLogout, tareas, setTareas }) => {
   const [activeSection, setActiveSection] = useState('inicio');
-  const [menuOpen, setMenuOpen] = useState(false);
 
   const renderContent = () => {
     switch (activeSection) {
@@ -32,22 +30,18 @@ const ProfesorDashboard = ({ user, onLogout, tareas, setTareas }) => {
       <Header
         user={user}
         onLogout={onLogout}
-        menuOpen={menuOpen}
-        setMenuOpen={setMenuOpen}
       />
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="flex gap-8">
-          <Sidebar
-            activeSection={activeSection}
-            setActiveSection={setActiveSection}
-            menuOpen={menuOpen}
-            setMenuOpen={setMenuOpen}
-            isProfesor={true}
-          />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-6">
+        <Sidebar
+          activeSection={activeSection}
+          setActiveSection={setActiveSection}
+          isProfesor={true}
+        />
 
-          <main className="flex-1">{renderContent()}</main>
-        </div>
+        <main className="mt-4 sm:mt-6">
+          {renderContent()}
+        </main>
       </div>
     </div>
   );
